a25eed7306e3ae052a902021e47f9c6edaaf164e
Timothy Wei <timswei@gmail.com>
2024-07-30 14:00:52 -0700

    add half working server

--- app.py
+++ app.py
@@ -8,12 +8,22 @@ import som
 import supervision as sv
 import torch
 from segment_anything import sam_model_registry
+from flask import Flask, request, jsonify
 
 from sam_utils import sam_interactive_inference, sam_inference
 from utils import postprocess_masks, Visualizer
+from flask import Flask, request, jsonify
+import numpy as np
+import cv2
+import base64
+from typing import Dict, List, Tuple, Any
 
-HOME = os.getenv("HOME")
+app = Flask(__name__)
+
+ANNOTATED_IMAGE_KEY = "annotated_image"
+DETECTIONS_KEY = "detections"
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+HOME = os.getenv("HOME")
 
 SAM_CHECKPOINT = os.path.join(HOME, "app/weights/sam_vit_h_4b8939.pth")
 # SAM_CHECKPOINT = "weights/sam_vit_h_4b8939.pth"
@@ -21,37 +31,15 @@ SAM_MODEL_TYPE = "vit_h"
 
 ANNOTATED_IMAGE_KEY = "annotated_image"
 DETECTIONS_KEY = "detections"
-MARKDOWN = """
-<div align='center'>
-    <h1>
-        <img 
-            src='https://som-gpt4v.github.io/website/img/som_logo.png' 
-            style='height:50px; display:inline-block'
-        />  
-        Set-of-Mark (SoM) Prompting Unleashes Extraordinary Visual Grounding in GPT-4V
-    </h1>
-    <br>
-    [<a href="https://arxiv.org/abs/2109.07529"> arXiv paper </a>] 
-    [<a href="https://som-gpt4v.github.io"> project page </a>]
-    [<a href="https://github.com/roboflow/set-of-mark"> python package </a>]
-    [<a href="https://github.com/microsoft/SoM"> code </a>]
-</div>
-
-## 🚧 Roadmap
-
-- [ ] Support for alphabetic labels
-- [ ] Support for Semantic-SAM (multi-level)
-- [ ] Support for mask filtering based on granularity
-"""
-
 SAM = sam_model_registry[SAM_MODEL_TYPE](checkpoint=SAM_CHECKPOINT).to(device=DEVICE)
-
 
 def inference(
     image_and_mask: Dict[str, np.ndarray],
     annotation_mode: List[str],
     mask_alpha: float
 ) -> Tuple[Tuple[np.ndarray, List[Tuple[np.ndarray, str]]], Dict[str, Any]]:
+    print("Inference")
+    print(image_and_mask)
     image = image_and_mask['image']
     mask = cv2.cvtColor(image_and_mask['mask'], cv2.COLOR_RGB2GRAY)
     is_interactive = not np.all(mask == 0)
@@ -83,127 +71,51 @@ def inference(
     }
     return (annotated_image, []), state
 
+def decode_image(image_str):
+    nparr = np.frombuffer(base64.b64decode(image_str), np.uint8)
+    return cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-def prompt(
-    message: str,
-    history: List[List[str]],
-    state: Dict[str, Any],
-    api_key: Optional[str]
-) -> str:
-    if api_key == "":
-        return "⚠️ Please set your OpenAI API key first"
-    if state is None or ANNOTATED_IMAGE_KEY not in state:
-        return "⚠️ Please generate SoM visual prompt first"
-    return som.prompt_image(
-        api_key=api_key,
-        image=cv2.cvtColor(state[ANNOTATED_IMAGE_KEY], cv2.COLOR_BGR2RGB),
-        prompt=message
-    )
+@app.route('/api/inference', methods=['POST'])
+def inference_api():
+    try:
+        # Get the JSON data from the request
+        data = request.get_json()
+        image = decode_image(data['image'])
+        
+        # Decode the image and mask from base64
+        image_and_mask = {
+            'image': image,
+            'mask': np.zeros_like(image)
+        }
+        
+        annotation_mode = data['annotation_mode']
+        mask_alpha = data['mask_alpha']
+        
+        # Call the inference function
+        (annotated_image, _), _ = inference(image_and_mask, annotation_mode, mask_alpha)        
+        # Encode the result image back to base64
+        _, buffer = cv2.imencode('.jpg', annotated_image)
+        result_image_str = base64.b64encode(buffer).decode('utf-8')
+        
+        # Prepare the response
+        response = {
+            'result_image': result_image_str,
+        }
+        
+        return jsonify(response)
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
+if __name__ == '__main__':
+    # image = cv2.imread('test.png')
+    # image_and_mask = {
+    #     'image': image,
+    #     'mask': np.zeros_like(image)
+    # }
+    # mask_alpha = 0.05
+    # annotation_mode = ["Mark", "Polygon"]
+    # (annotated_image, _), _ = inference(image_and_mask, annotation_mode, mask_alpha)
 
-def on_image_input_clear():
-    return None, {}
-
-
-def highlight(
-    state: Dict[str, Any],
-    history: List[List[str]]
-) -> Optional[Tuple[np.ndarray, List[Tuple[np.ndarray, str]]]]:
-    if DETECTIONS_KEY not in state or ANNOTATED_IMAGE_KEY not in state:
-        return None
-
-    detections: sv.Detections = state[DETECTIONS_KEY]
-    annotated_image: np.ndarray = state[ANNOTATED_IMAGE_KEY]
-
-    if len(history) == 0:
-        return None
-
-    text = history[-1][-1]
-    relevant_masks = som.extract_relevant_masks(
-        text=text,
-        detections=detections
-    )
-    relevant_masks = [
-        (mask, mark)
-        for mark, mask
-        in relevant_masks.items()
-    ]
-    return annotated_image, relevant_masks
-
-
-image_input = gr.Image(
-    label="Input",
-    type="numpy",
-    tool="sketch",
-    interactive=True,
-    brush_radius=20.0,
-    brush_color="#FFFFFF",
-    height=512
-)
-checkbox_annotation_mode = gr.CheckboxGroup(
-    choices=["Mark", "Polygon", "Mask", "Box"],
-    value=['Mark'],
-    label="Annotation Mode")
-slider_mask_alpha = gr.Slider(
-    minimum=0,
-    maximum=1,
-    value=0.05,
-    label="Mask Alpha")
-image_output = gr.AnnotatedImage(
-    label="SoM Visual Prompt",
-    color_map={
-        str(i): sv.ColorPalette.default().by_idx(i).as_hex()
-        for i in range(64)
-    },
-    height=512
-)
-openai_api_key = gr.Textbox(
-    show_label=False,
-    placeholder="Before you start chatting, set your OpenAI API key here",
-    lines=1,
-    type="password")
-chatbot = gr.Chatbot(
-    label="GPT-4V + SoM",
-    height=256)
-generate_button = gr.Button("Generate Marks")
-highlight_button = gr.Button("Highlight Marks")
-
-with gr.Blocks() as demo:
-    gr.Markdown(MARKDOWN)
-    inference_state = gr.State({})
-    with gr.Row():
-        with gr.Column():
-            image_input.render()
-            with gr.Accordion(
-                    label="Detailed prompt settings (e.g., mark type)",
-                    open=False):
-                with gr.Row():
-                    checkbox_annotation_mode.render()
-                with gr.Row():
-                    slider_mask_alpha.render()
-        with gr.Column():
-            image_output.render()
-            generate_button.render()
-            highlight_button.render()
-    with gr.Row():
-        openai_api_key.render()
-    with gr.Row():
-        gr.ChatInterface(
-            chatbot=chatbot,
-            fn=prompt,
-            additional_inputs=[inference_state, openai_api_key])
-
-    generate_button.click(
-        fn=inference,
-        inputs=[image_input, checkbox_annotation_mode, slider_mask_alpha],
-        outputs=[image_output, inference_state])
-    image_input.clear(
-        fn=on_image_input_clear,
-        outputs=[image_output, inference_state]
-    )
-    highlight_button.click(
-        fn=highlight,
-        inputs=[inference_state, chatbot],
-        outputs=[image_output])
-
-demo.queue().launch(debug=False, show_error=True)
+    # cv2.imwrite('annotated_image.png', annotated_image)
+    app.run(debug=True)
